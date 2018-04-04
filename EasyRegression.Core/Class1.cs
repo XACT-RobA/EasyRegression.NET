47159b0bf03a80c27ef1326a009c17f8e3699171
Robert Anderson <robertanderson1993@gmail.com>
2018-04-04 20:19:30 +0100

    Added project shell

---
+++ EasyRegression.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace EasyRegression.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ EasyRegression.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyRegression.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
